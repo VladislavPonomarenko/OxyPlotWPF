49b28e0820e70db2257800f562864c7cdf3982ff
Vlad Ponomarenko <vladponomarenko8@gmail.com>
2021-06-28 08:49:14 +0300

    Add project files.

---
+++ OxyPlotWPF/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OxyPlotWPF.ApplicationContext;
+using System.Windows;
+
+namespace OxyPlotWPF
+{
+    public partial class App : Application
+    {
+        private Bootstrapper _bootstrapper;
+
+        public App()
+        {
+            _bootstrapper = new Bootstrapper();
+        }
+    }
+}
---
+++ OxyPlotWPF/ApplicationContext/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using OxyPlotWPF.Screens.MainWindow;
+using System.Windows;
+
+namespace OxyPlotWPF.ApplicationContext
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
